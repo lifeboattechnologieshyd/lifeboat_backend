ac22924798311fd8b712ed8bee498743184999ee
ranjith <padidalaranjith@gmail.com>
2026-09-09 15:14:10 +0530

    email sending logic added.

--- config/settings/local.py
+++ config/settings/local.py
@@ -23,5 +23,5 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-FRONTEND_URL = "http://127.0.0.1:5174"
+FRONTEND_URL = "https://dev-web.lifeboattechnologies.com/"
 ISHVAA_EMAIL_API_KEY = 'ics_live_25I1njmlVD0OGiaL9orr4RxvTrgIO-JsR6HSRzkGcFY'
